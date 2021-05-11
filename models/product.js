229ba158a0550553759f2f21d4d29956452f4300
prerna bhatra <bhatraprerna061@gmail.com>
2021-05-11 11:40:00 +0530

    add product

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const userSchema=new mongoose.Schema({
+	item_name:
+	{
+		type:String,
+		require:true	
+	},
+	price:
+	{
+		type:Number,
+		require:true
+	}	
+},
+{
+	timestamps:true
+}
+);
+
+module.exports=mongoose.model('User',userSchema);
+
+
+
